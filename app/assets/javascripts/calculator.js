688be7d59c5d9cedf3866a0c212317cb05e03cac
Dharini Murugaprabhu <dharinishree@gmail.com>
2017-05-12 09:08:18 -0700

    cleanup ajax

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,31 +1,41 @@
-var hot = null;
-$(document).ready(function() {
-    $("#submission").hide();
-    //$('#start').on('click', displaySpreadsheet)
-    displaySpreadsheet();
+var XlsManager = function (opts = {}) {
+  this.originalSheet = null;
+  this.originalSheetContainerId = opts.originalSheetContainerId || "calculator";
 
-    $("#submission").on('click', evaluateSpredsheet)
+  this.evaluateSheet = null;
+  this.evaluateSheetContainerId =  opts.evaluateSheetContainerId || "calculatorEvaluated";
+}
 
-    function displaySpreadsheet() {
-      var data = Handsontable.helper.createEmptySpreadsheetData(10, 10);
-      var container = document.getElementById('calculator');
-      hot = new Handsontable(container, {
-        data: data,
-        minSpareCols: 1,
-        minSpareRows: 1,
-        rowHeaders: true,
-        colHeaders: true,
-        contextMenu: true,
-        colWidthsArray: 500
-      });
-      //$('#start').hide();
-      $("#submission").show();   
-    }
+XlsManager.prototype.displayOriginalSpreadsheet = function () {
+    data = Handsontable.helper.createEmptySpreadsheetData(10, 10);
+    var container = document.getElementById(this.originalSheetContainerId);
+    this.originalSheet = new Handsontable(container, {
+      data: data,
+      minSpareCols: 1,
+      minSpareRows: 1,
+      rowHeaders: true,
+      colHeaders: true,
+      contextMenu: true
+    });
+}
 
-    function evaluateSpredsheet(e, obj) {
-      var xlsData = hot.getData();
-      var colCount = hot.countCols()-hot.countEmptyCols();
+XlsManager.prototype.displayEvaluatedSpreadsheet = function (data) {
+    var container = document.getElementById(this.evaluateSheetContainerId);
+    this.evaluateSheet = new Handsontable(container, {
+      data: data,
+      minSpareCols: 1,
+      minSpareRows: 1,
+      rowHeaders: true,
+      colHeaders: true,
+      contextMenu: true
+    });
+}
 
+function evaluateSpredsheet(xlsManager) {
+    return function evaluateSpredsheet(e, obj) {
+      var xlsData = xlsManager.originalSheet.getData();
+      var colCount = xlsManager.originalSheet.countCols() - xlsManager.originalSheet.countEmptyCols();
+    
       $.ajax({
         url: '/calculators',
         method: 'POST',
@@ -34,17 +44,9 @@ $(document).ready(function() {
         data: JSON.stringify({xls_data: xlsData, col_count: colCount} ),
         success: function (result) {
             $("#calculatorEvaluated").html("");
-            var container1 = document.getElementById('calculatorEvaluated');
-            hot1 = new Handsontable(container1, {
-                data: result,
-                minSpareCols: 1,
-                minSpareRows: 1,
-                rowHeaders: true,
-                colHeaders: true,
-                contextMenu: true
-              }
-            );
             $('.nav-tabs a[href="#2a"]').tab('show')
+
+            xlsManager.displayEvaluatedSpreadsheet(result);
         },
         error: function (response, m) {
           var errorMesage = response.responseJSON.message;
@@ -54,4 +56,10 @@ $(document).ready(function() {
         }
       })
     }
+}
+
+$(document).ready(function() {
+    var xlsManager = new XlsManager();
+    xlsManager.displayOriginalSpreadsheet();
+    $("#submission").on('click', evaluateSpredsheet(xlsManager))
 })
