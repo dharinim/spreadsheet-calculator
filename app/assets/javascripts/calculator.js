c868cdc1ee4de5a88ddb060521e3b52151bf9090
Dharini Murugaprabhu <dharinishree@gmail.com>
2017-05-12 09:30:00 -0700

    Add frontend validation

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -31,30 +31,54 @@ XlsManager.prototype.displayEvaluatedSpreadsheet = function (data) {
     });
 }
 
+// Validate in frontend for matrix to be NxM without holes
+XlsManager.prototype.validateInput = function (data) {
+  var filledRows = this.originalSheet.countRows() - this.originalSheet.countEmptyRows();
+  var filledColumns = this.originalSheet.countCols() - this.originalSheet.countEmptyCols();
+
+  for (var row = 0; row < filledRows; row++) {
+    for (var col = 0; col < filledColumns; col++) {
+      if (data[row][col] == null) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+  
+}
+
 function evaluateSpredsheet(xlsManager) {
     return function evaluateSpredsheet(e, obj) {
       var xlsData = xlsManager.originalSheet.getData();
       var colCount = xlsManager.originalSheet.countCols() - xlsManager.originalSheet.countEmptyCols();
     
-      $.ajax({
-        url: '/calculators',
-        method: 'POST',
-        dataType: "json",
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify({xls_data: xlsData, col_count: colCount} ),
-        success: function (result) {
-            $("#calculatorEvaluated").html("");
-            $('.nav-tabs a[href="#2a"]').tab('show')
+      if (xlsManager.validateInput(xlsData) == true)  {
+        $.ajax({
+          url: '/calculators',
+          method: 'POST',
+          dataType: "json",
+          contentType: "application/json; charset=utf-8",
+          data: JSON.stringify({xls_data: xlsData, col_count: colCount} ),
+          success: function (result) {
+              $("#calculatorEvaluated").html("");
+              $('.nav-tabs a[href="#2a"]').tab('show')
 
-            xlsManager.displayEvaluatedSpreadsheet(result);
-        },
-        error: function (response, m) {
-          var errorMesage = response.responseJSON.message;
-          $("#flash").html(errorMesage);
-          $("#flash").removeClass("btn-info");
-          $("#flash").addClass("btn-danger")
-        }
-      })
+              xlsManager.displayEvaluatedSpreadsheet(result);
+          },
+          error: function (response, m) {
+            var errorMesage = response.responseJSON.message;
+            $("#flash").html(errorMesage);
+            $("#flash").removeClass("btn-info");
+            $("#flash").addClass("btn-danger")
+          }
+        })        
+      } else {
+            $("#flash").html("Input data not valid: any NxM matrics in xls should not be null and have holes");
+            $("#flash").removeClass("btn-info");
+            $("#flash").addClass("btn-danger")
+      }
+
     }
 }
 
